598f935994c8c7ed5a54efdc5bd4c0b72ac5be95
Ulysses Lizarraga <ulises.lizarraga@gmail.com>
2020-12-19 21:26:25 -0800

    added chat message download capabilities; reformatted to class

---
+++ discord_analytics/analytics_adapter.py
@@ -0,0 +1,81 @@
+import pathlib
+import subprocess
+import json
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+from collections import defaultdict
+from libdisc import colorstr
+
+class Discord_Analytics():
+
+    def __init__(self, config_file='config.json', show_top=30):
+        """
+        Initializes class configuration parameters
+        """
+        self.adapter_path = pathlib.Path(__file__).parent.absolute()
+        config_paths = [str(self.adapter_path)+'\\'+config_file, config_file]
+        self.show_top = show_top
+        self.export_filename = 'chat_messages.html'
+        self.clr = colorstr.bcolors()
+        config_dict = None
+
+        for config_path in config_paths:
+            try:
+                config_dict = json.load(open(config_path))
+                if config_dict:
+                    break
+            except:
+                pass
+        if not config_dict:
+            print(f"\n{self.clr.FAIL}[ERROR]{self.clr.ENDC} Empty or non-existent config file in either of paths:")
+            print(f"{config_paths}")
+            return
+
+        self.discord_chat_cli = config_dict["discord_chat_cli"]
+        self.channel_id = config_dict["channel_id"]
+        self.token = config_dict["token"]
+        self.export_cmd = self.discord_chat_cli+' export -t '+self.token+' -c '+self.channel_id+' -o '+str(self.adapter_path)+'\\'+self.export_filename
+
+    def download_chat_messages(self):
+        """
+        Downloads chat messages
+        """
+        subprocess.call(self.export_cmd)
+
+    def compute_chat_analytics(self, plotting=False):
+        """
+        Computes chat analytics.
+
+        Computes rank of words and characters used by users.
+        """
+
+        with open(str(self.adapter_path)+'\\'+self.export_filename, 'r', encoding="utf8") as f:
+            contents = f.read()
+            soup = BeautifulSoup(contents, 'lxml')
+            chatlog = soup.find_all('div', class_='chatlog__messages')
+            char_count = defaultdict(int)
+            word_dict = defaultdict(int)
+            word_counter = defaultdict(int)
+            for i in range(len(chatlog)):
+                person = chatlog[i].find_all('span', class_='chatlog__author-name')[0].get_text()
+                for message in chatlog[i].find_all('span', class_='preserve-whitespace'):
+                    char_count[person] = char_count[person] + len(message.get_text())
+                    word_dict[person] = word_dict[person] + len(message.get_text().split())
+                    for w in message.get_text().split():
+                        word_counter[w] += 1
+
+            plt.bar(char_count.keys(), char_count.values(), color='g',label='Character count')
+            plt.bar(word_dict.keys(), word_dict.values(), color='r',label='Word count')
+            plt.xlabel("Players")
+            plt.grid()
+            plt.legend()
+            
+            popular_words = sorted(word_counter, key = word_counter.get, reverse = True)
+            tops = popular_words[:self.show_top]
+            for p, count in sorted(char_count.items(), key=lambda item: item[1], reverse=True):
+                print(f"El {p}: {count}")
+            for i in range(len(tops)):
+                print(f"#{i}: {tops[i]}, ocurrances: {word_counter[tops[i]]}")
+
+            if plotting:
+                plt.show()
