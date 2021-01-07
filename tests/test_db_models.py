45637fd9fcb085da08516be536a0d9a2f3d57a36
Ulysses Lizarraga <ulises.lizarraga@gmail.com>
2021-01-07 01:03:39 -0800

    Commenting out script - need to reformat into unit test cases

--- tests/test_db_models.py
+++ tests/test_db_models.py
@@ -1,21 +1,21 @@
-import context
-import pytest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from libdisc.models.base_mixin import Base
-from libdisc.models.gif import Gif
-from libdisc.models.user import User
-from libdisc.models.message import Message
+# import context
+# import pytest
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from libdisc.models.base_mixin import Base
+# from libdisc.models.gif import Gif
+# from libdisc.models.user import User
+# from libdisc.models.message import Message
 
-dbpath='sqlite:///gif.db'
-engine = create_engine(dbpath, pool_recycle=600)
-Base.metadata.create_all(engine)
-curr_session = sessionmaker(bind=engine)()
+# dbpath='sqlite:///gif.db'
+# engine = create_engine(dbpath, pool_recycle=600)
+# Base.metadata.create_all(engine)
+# curr_session = sessionmaker(bind=engine)()
 
-# people = ["demmy", "cris", "juan", "ulises", "wang"]
-people = ["ulises"]
+# # people = ["demmy", "cris", "juan", "ulises", "wang"]
+# people = ["ulises"]
 
-for person in people:
-    user_id = User.get_or_create(curr_session, person)
-    timestamp = Gif.read_gif_entry(curr_session, user_id)
-    print(timestamp)
+# for person in people:
+#     user_id = User.get_or_create(curr_session, person)
+#     timestamp = Gif.read_gif_entry(curr_session, user_id)
+#     print(timestamp)
