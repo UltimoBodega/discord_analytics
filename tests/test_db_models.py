3b77897d27f96fb01be4f4e1943d22ecd7832ca5
Ulysses Lizarraga <ulises.lizarraga@gmail.com>
2021-01-07 11:22:29 -0800

    Added test case for upsert and get Gif db methods

--- tests/test_db_models.py
+++ tests/test_db_models.py
@@ -1,21 +1,64 @@
-# import context
-# import pytest
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from libdisc.models.base_mixin import Base
-# from libdisc.models.gif import Gif
-# from libdisc.models.user import User
-# from libdisc.models.message import Message
+import random
+import context
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-# dbpath='sqlite:///gif.db'
-# engine = create_engine(dbpath, pool_recycle=600)
-# Base.metadata.create_all(engine)
-# curr_session = sessionmaker(bind=engine)()
+from discord_analytics.analytics_engine import AnalyticsEngine
+from libdisc.database_manager import DatabaseManager
+from libdisc.discord_manager import DiscordManager
+from libdisc.media_manager import MediaManager
+from libdisc.models.base_mixin import BaseModel
 
-# # people = ["demmy", "cris", "juan", "ulises", "wang"]
-# people = ["ulises"]
+def test_gif_db() -> None:
+    """
+    Tests the 'upsert' and 'get' method of the Gif class.
 
-# for person in people:
-#     user_id = User.get_or_create(curr_session, person)
-#     timestamp = Gif.read_gif_entry(curr_session, user_id)
-#     print(timestamp)
+    Test procedure:
+    1- Reads empty db.
+    2- Inserts data to empty db using upsert.
+    3- Reads db.
+    4- Updates data to db using upsert.
+    5- Reads updated db.
+    """
+    preferences = [("John", "Matrix", 150), ("Jane", "The Notebook", 150000),
+            ("Bob", "Stranger Things", 0), ("Mike", "Iron Man", 10000)]
+    result = {}
+    select_person = "Bob"
+    update_data = (select_person, "Twin Peaks", 75000)
+    filename = "test_db_models.db"
+    db_url = "sqlite:///"+filename
+    gif_dummy_key = ""
+    output = {'Read empty': ('', 0), 'Initial preference': ('Stranger Things', 0), 'Read update': ('Twin Peaks', 75000)}
+
+    engine = create_engine(db_url, pool_recycle=299, pool_pre_ping=True)
+    BaseModel.metadata.create_all(engine)
+    session_maker =  sessionmaker(bind=engine)
+    curr_session = session_maker()
+    analytics_engine = AnalyticsEngine(db_session=curr_session)
+    database_manager = DatabaseManager(db_session=curr_session)
+    media_manager = MediaManager(gif_dummy_key)
+    discord_manager = DiscordManager(db_manager=database_manager, analytics_engine=analytics_engine,
+                                    media_manager=media_manager)
+
+
+    # Query empty DB for Bob - should return nothing
+    result["Read empty"] = discord_manager.db_manager.get_last_gif_preference(select_person)
+    # discord_manager.db_manager.upsert_new_gif_entry()
+
+    for preference in preferences:
+        (user_name, keyword, timestamp) = preference
+        discord_manager.db_manager.upsert_new_gif_entry(user_name, keyword, timestamp)
+
+    # Read for Bob - should initial preference
+    result["Initial preference"] = discord_manager.db_manager.get_last_gif_preference(select_person)
+
+    # Update Bob's preferences
+    (user_name, keyword, timestamp) = update_data
+    discord_manager.db_manager.upsert_new_gif_entry(user_name, keyword, timestamp)
+
+    # Read for Bob - should initial preference
+    result["Read update"] = discord_manager.db_manager.get_last_gif_preference(select_person)
+    
+    os.remove(filename)
+    assert(result==output)
