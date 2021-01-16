748d6c9b6de64b55b3a78df198d13b6cef1d7452
Zhe Wang <hellozhewang@gmail.com>
2021-01-16 14:14:48 -0800

    take4

--- build.sh
+++ build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
 export PYTHONPATH=$PWD/src
-echo $PYTHONPATH
 export MYPYPATH=$PYTHONPATH
+pip3 install --upgrade pip wheel
 pip3 install -r requirements.txt
-mypy src
-mypy tests
-flake8 src
-flake8 tests
+mypy src tests
+flake8 src tests
 pytest tests
 printf "\n\n ---Build successfully completed--- \n\n"
